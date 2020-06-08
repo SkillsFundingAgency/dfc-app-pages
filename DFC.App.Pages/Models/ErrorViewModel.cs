a45ef0253125cf74cdba4c545f739ec519e3b6c8
Himanshu Trivedi <himanshu.trivedi@hotmail.com>
2020-06-08 13:35:13 +0100

    Initial Commit New repo

---
+++ DFC.App.Pages/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DFC.App.Pages.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
