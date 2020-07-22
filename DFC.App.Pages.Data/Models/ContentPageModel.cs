289dd9fff0eaf64209e4ae3369e012921fa411cb
Ian Crisp <icrisp@program-systems.co.uk>
2020-07-22 08:14:16 +0100

    Updated cosmos nuget

--- DFC.App.Pages.Data/Models/ContentPageModel.cs
+++ DFC.App.Pages.Data/Models/ContentPageModel.cs
@@ -1,5 +1,4 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
@@ -8,21 +7,14 @@ namespace DFC.App.Pages.Data.Models
     public class ContentPageModel : Compui.Cosmos.Models.ContentPageModel
     {
         [Required]
-        [JsonProperty(Order = -10)]
         public override string? PartitionKey => PageLocation;
 
-        public bool IsDefaultForPageLocation { get; set; }
-
         public override string? PageLocation { get; set; } = "/missing-location";
+
+        public new Guid? Version { get; set; }
 
         public new string? Content { get; set; }
 
         public IList<ContentItemModel>? ContentItems { get; set; }
-
-        [Display(Name = "Breadcrumb Title")]
-        public new string? BreadcrumbTitle { get; set; }
-
-        [JsonProperty(Order = -10)]
-        public new Guid? Version { get; set; }
     }
 }
