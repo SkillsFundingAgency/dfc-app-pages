01a43a5120c2eb4e59c782872be5137ba370caf1
Ian Crisp <icrisp@program-systems.co.uk>
2020-07-23 13:56:49 +0100

    correct sitemap to prevent // in paths

--- DFC.App.Pages.UnitTests/ControllerTests/SitemapControllerTests/SitemapControllerViewTests.cs
+++ DFC.App.Pages.UnitTests/ControllerTests/SitemapControllerTests/SitemapControllerViewTests.cs
@@ -15,20 +15,26 @@ namespace DFC.App.Pages.UnitTests.ControllerTests.SitemapControllerTests
         public async Task SitemapControllerViewReturnsSuccess()
         {
             // Arrange
-            const int resultsCount = 3;
+            const int resultsCount = 5;
             var expectedResults = A.CollectionOfFake<ContentPageModel>(resultsCount);
             var controller = BuildSitemapController();
 
             expectedResults[0].IncludeInSitemap = true;
-            expectedResults[0].PageLocation = "default-location";
+            expectedResults[0].PageLocation = "/default-location";
             expectedResults[0].CanonicalName = "default-article";
             expectedResults[0].IsDefaultForPageLocation = true;
             expectedResults[1].IncludeInSitemap = false;
-            expectedResults[1].PageLocation = "default-location";
+            expectedResults[1].PageLocation = "/default-location";
             expectedResults[1].CanonicalName = "not-in-sitemap";
             expectedResults[2].IncludeInSitemap = true;
-            expectedResults[2].PageLocation = "default-location";
+            expectedResults[2].PageLocation = "/default-location";
             expectedResults[2].CanonicalName = "in-sitemap";
+            expectedResults[3].IncludeInSitemap = true;
+            expectedResults[3].PageLocation = "/";
+            expectedResults[3].CanonicalName = "slash-location";
+            expectedResults[4].IncludeInSitemap = true;
+            expectedResults[4].PageLocation = string.Empty;
+            expectedResults[4].CanonicalName = "empty-location";
 
             A.CallTo(() => FakeContentPageService.GetAllAsync()).Returns(expectedResults);
 
