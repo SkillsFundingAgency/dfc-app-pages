4f637e2c70108a69a44d6409ae677c198f90ee46
Ian Crisp <icrisp@program-systems.co.uk>
2020-08-25 16:12:02 +0100

    Updated unit tests to improve code coverage

--- DFC.App.Pages.Services.CacheContentService.UnitTests/WebhooksServiceTests/WebhooksServiceProcessContentItemAsyncTests.cs
+++ DFC.App.Pages.Services.CacheContentService.UnitTests/WebhooksServiceTests/WebhooksServiceProcessContentItemAsyncTests.cs
@@ -1,4 +1,5 @@
-﻿using DFC.App.Pages.Data.Models;
+﻿using DFC.App.Pages.Data.Common;
+using DFC.App.Pages.Data.Models;
 using FakeItEasy;
 using System;
 using System.Collections.Generic;
@@ -52,7 +53,7 @@ namespace DFC.App.Pages.Services.CacheContentService.UnitTests.WebhooksServiceTe
             const HttpStatusCode expectedResponse = HttpStatusCode.OK;
             var exptectedGuidList = new List<Guid> { ContentIdForUpdate, Guid.NewGuid() };
             var expectedValidPagesApiContentItemModel = BuildValidPagesApiContentItemDataModel();
-            var expectedValidContentPageModel = BuildValidContentPageModel();
+            var expectedValidContentPageModel = BuildValidContentPageModel(Constants.ContentTypePageLocation);
             var expectedValidContentItemModel = BuildValidContentItemModel(ContentItemIdForUpdate);
             var url = new Uri("https://somewhere.com");
             var service = BuildWebhooksService();
@@ -60,7 +61,6 @@ namespace DFC.App.Pages.Services.CacheContentService.UnitTests.WebhooksServiceTe
             A.CallTo(() => FakeContentCacheService.GetContentIdsContainingContentItemId(A<Guid>.Ignored)).Returns(exptectedGuidList);
             A.CallTo(() => FakeCmsApiService.GetContentItemAsync<PagesApiContentItemModel>(A<Uri>.Ignored)).Returns(expectedValidPagesApiContentItemModel);
             A.CallTo(() => FakeContentPageService.GetByIdAsync(A<Guid>.Ignored)).Returns(expectedValidContentPageModel);
-            A.CallTo(() => FakeMapper.Map(A<PagesApiContentItemModel>.Ignored, A<ContentItemModel>.Ignored)).Returns(expectedValidContentItemModel);
             A.CallTo(() => FakeEventMessageService.UpdateAsync(A<ContentPageModel>.Ignored)).Returns(HttpStatusCode.OK);
 
             // Act
@@ -70,7 +70,7 @@ namespace DFC.App.Pages.Services.CacheContentService.UnitTests.WebhooksServiceTe
             A.CallTo(() => FakeContentCacheService.GetContentIdsContainingContentItemId(A<Guid>.Ignored)).MustHaveHappenedOnceExactly();
             A.CallTo(() => FakeCmsApiService.GetContentItemAsync<PagesApiContentItemModel>(A<Uri>.Ignored)).MustHaveHappenedOnceExactly();
             A.CallTo(() => FakeContentPageService.GetByIdAsync(A<Guid>.Ignored)).MustHaveHappenedOnceOrMore();
-            A.CallTo(() => FakeMapper.Map(A<PagesApiContentItemModel>.Ignored, A<ContentItemModel>.Ignored)).MustHaveHappenedOnceOrMore();
+            A.CallTo(() => FakeMapper.Map(A<PagesApiContentItemModel>.Ignored, A<ContentItemModel>.Ignored)).MustNotHaveHappened();
             A.CallTo(() => FakeEventMessageService.UpdateAsync(A<ContentPageModel>.Ignored)).MustHaveHappenedOnceOrMore();
             A.CallTo(() => FakeEventMessageService.CreateAsync(A<ContentPageModel>.Ignored)).MustNotHaveHappened();
             A.CallTo(() => FakeEventMessageService.DeleteAsync(A<Guid>.Ignored)).MustNotHaveHappened();
