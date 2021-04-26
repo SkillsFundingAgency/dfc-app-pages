ca78451e8f32464d1c51c121fe7cd15df7b73b9f
Ian Crisp <icrisp@program-systems.co.uk>
2021-04-26 12:35:46 +0100

    Added show breadcrumb indicator from CMS

--- DFC.App.Pages.UnitTests/ControllerTests/PagesControllerTests/PagesControllerDocumentTests.cs
+++ DFC.App.Pages.UnitTests/ControllerTests/PagesControllerTests/PagesControllerDocumentTests.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Net;
+using System.Net.Mime;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -29,6 +30,7 @@ namespace DFC.App.Pages.UnitTests.ControllerTests.PagesControllerTests
             {
                 PageLocation = "/" + pageRequestModel.Location1,
                 CanonicalName = pageRequestModel.Location2,
+                ShowBreadcrumb = true,
             };
             var controller = BuildPagesController(mediaTypeName);
             var expectedModel = new DocumentViewModel
@@ -89,8 +91,23 @@ namespace DFC.App.Pages.UnitTests.ControllerTests.PagesControllerTests
                 Location1 = "a-location-name",
                 Location2 = "an-article-name",
             };
-            var expectedResult = new ContentPageModel() { PageLocation = "/" + pageRequestModel.Location1, CanonicalName = pageRequestModel.Location2 };
-            BreadcrumbViewModel? expectedBreadcrumbModel = null;
+            var expectedResult = new ContentPageModel() { PageLocation = "/" + pageRequestModel.Location1, CanonicalName = pageRequestModel.Location2, ShowBreadcrumb = true, };
+            var expectedBreadcrumbModel = new BreadcrumbViewModel
+            {
+                Breadcrumbs = new List<BreadcrumbItemViewModel>
+                {
+                    new BreadcrumbItemViewModel
+                    {
+                        Route = "/",
+                        Title = "Root",
+                    },
+                    new BreadcrumbItemViewModel
+                    {
+                        Route = "/a-route",
+                        Title = "A title",
+                    },
+                },
+            };
             var controller = BuildPagesController(mediaTypeName);
 
             A.CallTo(() => FakePagesControlerHelpers.GetContentPageAsync(A<string>.Ignored, A<string>.Ignored)).Returns(expectedResult);
@@ -192,8 +209,23 @@ namespace DFC.App.Pages.UnitTests.ControllerTests.PagesControllerTests
                 Location1 = "a-location-name",
                 Location2 = "an-article-name",
             };
-            var expectedResult = new ContentPageModel() { PageLocation = "/" + pageRequestModel.Location1, CanonicalName = pageRequestModel.Location2 };
-            BreadcrumbViewModel? expectedBreadcrumbModel = null;
+            var expectedResult = new ContentPageModel() { PageLocation = "/" + pageRequestModel.Location1, CanonicalName = pageRequestModel.Location2, ShowBreadcrumb = true, };
+            var expectedBreadcrumbModel = new BreadcrumbViewModel
+            {
+                Breadcrumbs = new List<BreadcrumbItemViewModel>
+                {
+                    new BreadcrumbItemViewModel
+                    {
+                        Route = "/",
+                        Title = "Root",
+                    },
+                    new BreadcrumbItemViewModel
+                    {
+                        Route = "/a-route",
+                        Title = "A title",
+                    },
+                },
+            };
             var controller = BuildPagesController(mediaTypeName);
 
             A.CallTo(() => FakePagesControlerHelpers.GetContentPageAsync(A<string>.Ignored, A<string>.Ignored)).Returns(expectedResult);
@@ -211,6 +243,53 @@ namespace DFC.App.Pages.UnitTests.ControllerTests.PagesControllerTests
             var statusResult = Assert.IsType<StatusCodeResult>(result);
 
             A.Equals((int)HttpStatusCode.NotAcceptable, statusResult.StatusCode);
+
+            controller.Dispose();
+        }
+
+        [Fact]
+        public async Task PagesControllerDocumentHtmlReturnsSuccessForDoNotShowBreadcrumb()
+        {
+            // Arrange
+            var pageRequestModel = new PageRequestModel
+            {
+                Location1 = "a-location-name",
+                Location2 = "an-article-name",
+            };
+            var expectedResult = new ContentPageModel()
+            {
+                PageLocation = "/" + pageRequestModel.Location1,
+                CanonicalName = pageRequestModel.Location2,
+                ShowBreadcrumb = false,
+            };
+            var controller = BuildPagesController(MediaTypeNames.Text.Html);
+            var expectedModel = new DocumentViewModel
+            {
+                DocumentId = Guid.NewGuid(),
+                CanonicalName = "a-canonical-name",
+                PartitionKey = "partition-key",
+                Version = Guid.NewGuid(),
+                IncludeInSitemap = true,
+                Url = new Uri("https://somewhere.com", UriKind.Absolute),
+                Content = new Microsoft.AspNetCore.Html.HtmlString("some content"),
+                LastReviewed = DateTime.Now,
+            };
+
+            A.CallTo(() => FakePagesControlerHelpers.GetContentPageAsync(A<string>.Ignored, A<string>.Ignored)).Returns(expectedResult);
+            A.CallTo(() => FakeMapper.Map<DocumentViewModel>(A<ContentPageModel>.Ignored)).Returns(expectedModel);
+
+            // Act
+            var result = await controller.Document(pageRequestModel).ConfigureAwait(false);
+
+            // Assert
+            A.CallTo(() => FakePagesControlerHelpers.GetContentPageAsync(A<string>.Ignored, A<string>.Ignored)).MustHaveHappenedOnceExactly();
+            A.CallTo(() => FakeMapper.Map<DocumentViewModel>(A<ContentPageModel>.Ignored)).MustHaveHappenedOnceExactly();
+            A.CallTo(() => FakeMapper.Map<BreadcrumbViewModel>(A<ContentPageModel>.Ignored)).MustNotHaveHappened();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+            _ = Assert.IsAssignableFrom<DocumentViewModel>(viewResult.ViewData.Model);
+            var model = viewResult.ViewData.Model as DocumentViewModel;
+            Assert.Equal(expectedModel, model);
 
             controller.Dispose();
         }
