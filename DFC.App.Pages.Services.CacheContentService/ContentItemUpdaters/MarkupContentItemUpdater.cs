892ea9d39b6623d16363c8df9e7dcee1a92ef575
Ian Crisp <icrisp@program-systems.co.uk>
2021-04-29 14:57:18 +0100

    Updated dynamic content updater from webhook

--- DFC.App.Pages.Services.CacheContentService/ContentItemUpdaters/MarkupContentItemUpdater.cs
+++ DFC.App.Pages.Services.CacheContentService/ContentItemUpdaters/MarkupContentItemUpdater.cs
@@ -1,9 +1,12 @@
-﻿using DFC.App.Pages.Data.Models;
+﻿using DFC.App.Pages.Data.Common;
+using DFC.App.Pages.Data.Contracts;
+using DFC.App.Pages.Data.Models;
+using DFC.App.Pages.Data.Models.CmsApiModels;
 using DFC.Content.Pkg.Netcore.Data.Contracts;
 using System;
 using System.Threading.Tasks;
 
-namespace DFC.App.Pages.Data.Contracts
+namespace DFC.App.Pages.Services.CacheContentService.ContentItemUpdaters
 {
     public class MarkupContentItemUpdater<T> : IMarkupContentItemUpdater<T>
         where T : class, IBaseContentItemModel, ICmsApiMarkupContentItem
@@ -19,13 +22,53 @@ namespace DFC.App.Pages.Data.Contracts
         {
             _ = contentItemModel ?? throw new ArgumentNullException(nameof(contentItemModel));
 
-            var cmsApiHtmlModel = await cmsApiService.GetContentItemAsync<T>(url).ConfigureAwait(false);
+            var cmsApiModel = await cmsApiService.GetContentItemAsync<T>(url).ConfigureAwait(false);
 
-            if (cmsApiHtmlModel != null)
+            if (cmsApiModel != null)
             {
-                contentItemModel.Title = cmsApiHtmlModel.Title;
-                contentItemModel.Content = cmsApiHtmlModel.Content;
-                contentItemModel.HtmlBody = cmsApiHtmlModel.HtmlBody;
+                switch (contentItemModel.ContentType)
+                {
+                    case Constants.ContentTypeHtml:
+                        if (cmsApiModel is CmsApiHtmlModel cmsApiHtmlModel)
+                        {
+                            contentItemModel.Title = cmsApiHtmlModel.Title;
+                            contentItemModel.Content = cmsApiHtmlModel.Content;
+                            contentItemModel.HtmlBody = cmsApiHtmlModel.HtmlBody;
+                        }
+
+                        break;
+                    case Constants.ContentTypeHtmlShared:
+                        if (cmsApiModel is CmsApiHtmlSharedModel cmsApiHtmlSharedModel)
+                        {
+                            contentItemModel.Title = cmsApiHtmlSharedModel.Title;
+                            contentItemModel.Content = cmsApiHtmlSharedModel.Content;
+                            contentItemModel.HtmlBody = cmsApiHtmlSharedModel.HtmlBody;
+                        }
+
+                        break;
+                    case Constants.ContentTypeSharedContent:
+                        if (cmsApiModel is CmsApiSharedContentModel cmsApiSharedContentModel)
+                        {
+                            contentItemModel.Title = cmsApiSharedContentModel.Title;
+                            contentItemModel.Content = cmsApiSharedContentModel.Content;
+                            contentItemModel.HtmlBody = cmsApiSharedContentModel.HtmlBody;
+                        }
+
+                        break;
+                    case Constants.ContentTypeForm:
+                        if (cmsApiModel is CmsApiFormModel cmsApiFormModel)
+                        {
+                            contentItemModel.Alignment = cmsApiFormModel.Alignment;
+                            contentItemModel.Ordinal = cmsApiFormModel.Ordinal;
+                            contentItemModel.Size = cmsApiFormModel.Size;
+                        }
+
+                        break;
+                }
+
+                contentItemModel.Alignment = cmsApiModel.Alignment;
+                contentItemModel.Ordinal = cmsApiModel.Ordinal;
+                contentItemModel.Size = cmsApiModel.Size;
                 contentItemModel.LastCached = DateTime.UtcNow;
 
                 return true;
