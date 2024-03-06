094cfd6af8622ee520fc4197746f81a71843dd10
hnandra <32364605+hnandra@users.noreply.github.com>
2024-03-06 19:38:44 +0000

    Added controller helper for pages

---
+++ DFC.App.Pages.Data/Contracts/IPagesControllerHelper.cs
@@ -0,0 +1,12 @@
+﻿using DFC.App.Pages.Data.Models;
+using System.Threading.Tasks;
+
+namespace DFC.App.Pages.Data.Contracts
+{
+    public interface IPagesControllerHelper
+    {
+        string GetPageUrl(string location, string article);
+
+        Task<ContentPageModel?> GetContentPageFromSharedAsync(string? location, string? article);
+    }
+}
