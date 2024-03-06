8ef52f38e8081b12d8852e95f228cf308f0eb6ee
hnandra <32364605+hnandra@users.noreply.github.com>
2024-03-06 19:52:14 +0000

    Updated Startup

--- DFC.App.Pages/Startup.cs
+++ DFC.App.Pages/Startup.cs
@@ -1,18 +1,11 @@
 ﻿using AutoMapper;
 using DFC.App.Pages.Cms.Data.Content;
 using DFC.App.Pages.Data.Contracts;
-using DFC.App.Pages.Data.Models;
 using DFC.App.Pages.Data.Models.ClientOptions;
-using DFC.App.Pages.Data.Models.CmsApiModels;
 using DFC.App.Pages.Extensions;
 using DFC.App.Pages.Helpers;
-using DFC.App.Pages.HostedServices;
 using DFC.App.Pages.HttpClientPolicies;
-using DFC.App.Pages.Models;
 using DFC.App.Pages.Services.AppRegistryService;
-using DFC.App.Pages.Services.CacheContentService;
-using DFC.App.Pages.Services.CacheContentService.ContentItemUpdaters;
-using DFC.App.Pages.Services.EventProcessorService;
 using DFC.Common.SharedContent.Pkg.Netcore;
 using DFC.Common.SharedContent.Pkg.Netcore.Constant;
 using DFC.Common.SharedContent.Pkg.Netcore.Infrastructure;
@@ -20,14 +13,9 @@ using DFC.Common.SharedContent.Pkg.Netcore.Infrastructure.Strategy;
 using DFC.Common.SharedContent.Pkg.Netcore.Interfaces;
 using DFC.Common.SharedContent.Pkg.Netcore.Model.ContentItems;
 using DFC.Common.SharedContent.Pkg.Netcore.Model.ContentItems.PageBreadcrumb;
-using DFC.Common.SharedContent.Pkg.Netcore.Model.ContentItems.Sitemap;
 using DFC.Common.SharedContent.Pkg.Netcore.Model.Response;
 using DFC.Common.SharedContent.Pkg.Netcore.RequestHandler;
-using DFC.Compui.Cosmos;
-using DFC.Compui.Cosmos.Contracts;
-using DFC.Compui.Subscriptions.Pkg.Netstandard.Extensions;
 using DFC.Compui.Telemetry;
-using DFC.Content.Pkg.Netcore.Data.Models.ClientOptions;
 using DFC.Content.Pkg.Netcore.Extensions;
 using GraphQL.Client.Abstractions;
 using GraphQL.Client.Http;
@@ -36,7 +24,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.Documents.Client;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -131,42 +118,18 @@ namespace DFC.App.Pages
             services.AddSingleton<ISharedContentRedisInterfaceStrategy<SitemapResponse>, PageSitemapStrategy>();
 
             services.AddScoped<ISharedContentRedisInterface, SharedContentRedis>();
+            services.AddScoped<IPagesControllerHelper, PagesControllerHelper>();
             services.ConfigureOptions<contentOptionsSetup>();
-
-            var cosmosDbConnectionContentPages = configuration.GetSection(CosmosDbContentPagesConfigAppSettings).Get<CosmosDbConnection>();
-            var cosmosRetryOptions = new RetryOptions { MaxRetryAttemptsOnThrottledRequests = 20, MaxRetryWaitTimeInSeconds = 60 };
-           services.AddContentPageServices<ContentPageModel>(cosmosDbConnectionContentPages, env.IsDevelopment(), cosmosRetryOptions);
 
             services.AddApplicationInsightsTelemetry();
             services.AddHttpContextAccessor();
-            services.AddTransient<IPagesControlerHelpers, PagesControlerHelpers>();
-            services.AddTransient<IEventMessageService<ContentPageModel>, EventMessageService<ContentPageModel>>();
-            services.AddTransient<ICacheReloadService, CacheReloadService>();
-            services.AddTransient<IWebhooksService, WebhooksService>();
-            services.AddTransient<IWebhookContentProcessor, WebhookContentProcessor>();
-            services.AddTransient<IPageLocatonUpdater, PageLocatonUpdater>();
-            services.AddTransient<IContentItemUpdater, ContentItemUpdater>();
-            services.AddTransient<IMarkupContentItemUpdater<CmsApiHtmlModel>, MarkupContentItemUpdater<CmsApiHtmlModel>>();
-            services.AddTransient<IMarkupContentItemUpdater<CmsApiHtmlSharedModel>, MarkupContentItemUpdater<CmsApiHtmlSharedModel>>();
-            services.AddTransient<IMarkupContentItemUpdater<CmsApiSharedContentModel>, MarkupContentItemUpdater<CmsApiSharedContentModel>>();
-            services.AddTransient<IMarkupContentItemUpdater<CmsApiFormModel>, MarkupContentItemUpdater<CmsApiFormModel>>();
-            services.AddTransient<IEventGridService, EventGridService>();
-            services.AddTransient<IEventGridClientService, EventGridClientService>();
             services.AddAutoMapper(typeof(Startup).Assembly);
-            services.AddSingleton(configuration.GetSection(nameof(CmsApiClientOptions)).Get<CmsApiClientOptions>() ?? new CmsApiClientOptions());
-            services.AddSingleton(configuration.GetSection(nameof(EventGridPublishClientOptions)).Get<EventGridPublishClientOptions>() ?? new EventGridPublishClientOptions());
             services.AddSingleton(configuration.GetSection(nameof(AppRegistryClientOptions)).Get<AppRegistryClientOptions>() ?? new AppRegistryClientOptions());
-            services.AddSingleton(configuration.GetSection(nameof(CacheReloadTimerOptions)).Get<CacheReloadTimerOptions>() ?? new CacheReloadTimerOptions());
             services.AddHostedServiceTelemetryWrapper();
-            services.AddSubscriptionBackgroundService(configuration);
-            services.AddHostedService<CacheReloadBackgroundService>();
-            services.AddHostedService<CacheReloadTimedHostedService>();
 
             const string AppSettingsPolicies = "Policies";
             var policyOptions = configuration.GetSection(AppSettingsPolicies).Get<PolicyOptions>() ?? new PolicyOptions();
             var policyRegistry = services.AddPolicyRegistry();
-
-           
 
             services.AddApiServices(configuration, policyRegistry);
 
