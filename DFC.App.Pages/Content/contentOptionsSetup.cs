38a438db25d745839710f8707caff058e5020e15
James <152189499+JamesPrior-07@users.noreply.github.com>
2024-06-12 09:04:03 +0100

    Fixed content mode value retrieval

---
+++ DFC.App.Pages/Content/contentOptionsSetup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace DFC.App.Pages.Cms.Data.Content
+{
+    public class contentOptionsSetup : IConfigureOptions<contentModeOptions>
+    {
+        private const string SectionName = "contentMode";
+        private readonly IConfiguration _configuration;
+
+        public contentOptionsSetup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void Configure(contentModeOptions options)
+        {
+            _configuration
+                .GetSection(SectionName)
+                .Bind(options);
+        }
+    }
+}
--- DFC.App.Pages/Startup.cs
+++ DFC.App.Pages/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using DFC.App.Pages.Cms.Data.Content;
 using DFC.App.Pages.Data.Contracts;
 using DFC.App.Pages.Data.Models.ClientOptions;
 using DFC.App.Pages.Extensions;
@@ -148,6 +149,7 @@ namespace DFC.App.Pages
             services.AddSingleton<ISharedContentRedisInterfaceStrategy<GetByPageApiResponse>, GetByIdPageApiStrategy>();
 
             services.AddScoped<ISharedContentRedisInterface, SharedContentRedis>();
+            services.ConfigureOptions<contentOptionsSetup>();
 
             services.AddApplicationInsightsTelemetry();
             services.AddHttpContextAccessor();
